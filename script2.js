1aeccac4175c0d90e5d4c58a2c6f0ee0676080de
Alex <muhammaddafa6279@gmail.com>
2024-10-27 20:20:16 +0700

    first commit

--- script2.js
+++ script2.js
@@ -4,10 +4,10 @@ document.addEventListener('DOMContentLoaded', function() {
     if (profileData) {
         document.getElementById('nama').textContent = profileData.nama || "Nama anda";
         document.getElementById('work').textContent = profileData.role || "Role anda";
-        document.getElementById('availability').value = profileData.availability || "";
-        document.getElementById('age').value = profileData.age || "";
-        document.getElementById('location').value = profileData.location || "";
-        document.getElementById('experience').value = profileData.experience || "";
-        document.getElementById('email').value = profileData.email || "";
+        document.getElementById('availability').value = profileData.availability;
+        document.getElementById('age').value = profileData.age;
+        document.getElementById('location').value = profileData.location;
+        document.getElementById('experience').value = profileData.experience;
+        document.getElementById('email').value = profileData.email;
     }
 });
